de6bcdb5cac32443a1bb715ccf0ce9edfaff2b69
novackirr <56271806+novackirr@users.noreply.github.com>
2022-12-16 22:35:12 +0300

    Edit title project/ Delete in Main.page test links

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -30,37 +30,6 @@
             </div>
         </div>
 
-
-        <div class="alert alert-info" role="alert">
-            Ниже ссылки на предполагаемые страницы
-        </div>
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <router-link to="/account" class="nav-link">
-                    Аккаунт
-                </router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/login" class="nav-link">
-                    Войти
-                </router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/register" class="nav-link">
-                    Регистрация
-                </router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/create" class="nav-link disabled">
-                    Создание нового варианта
-                </router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/info" class="nav-link">
-                    Информация о сайте
-                </router-link>
-            </li>
-        </ul>
     </div>
 </template>
 
